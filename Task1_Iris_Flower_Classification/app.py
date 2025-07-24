fe911c21dbc9aaa88da959d04e28b55e37b4b600
Nitin68BFH <87320694+Nitin68BFH@users.noreply.github.com>
2025-07-24 23:36:00 +0530

    Add files via upload

---
+++ Task1_Iris_Flower_Classification/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import numpy as np
+import joblib
+
+#loading model
+model = joblib.load('iris_model.pkl')
+class_names = ['Iris-setosa','Iris-versicolor','Iris-virginica']
+#user input function
+def user_input():
+    sepal_length = st.slider('Sepal Length (cm)' , 4.0,8.0,5.1)
+    sepal_width = st.slider('Sepal Width (cm)',2.0,4.5,3.5)
+
+    petal_length = st.slider('Petal Length (cm)',1.0,7.0,1.4)
+    petal_width = st.slider('Petal Width (cm)',0.1,2.5,0.2)
+
+    return np.array([[sepal_length , sepal_width , petal_length , petal_width]])
+
+#prediction function
+def predict_flower(data):
+    prediction = model.predict(data)[0]
+    return prediction
+
+# function for steamlit app
+def main():
+    st.title('🌸Iris Flower Prediction')
+
+    input_data = user_input()
+
+    if st.button('Predict'):
+        pred_class = predict_flower(input_data) 
+        st.success(f'Predicted Iris Class :{class_names[pred_class]}')
+
+if __name__ == '__main__':
+    main()
